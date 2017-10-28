c07e3236e2d2af3bc9b90b74172a0ab592c2440d
tashi21412 <tashi21412@gmail.com>
2017-10-28 19:04:29 -0400

    completed homepage, profile,form

--- WebApp/homepage.js
+++ WebApp/homepage.js
@@ -1,7 +1,7 @@
 window.onload = function(){
 	
 	document.getElementById("openNav").addEventListener("click",w3_open, false);
-document.getElementById("closeNav").addEventListener("click",w3_close, false);
+	document.getElementById("closeNav").addEventListener("click",w3_close, false);
 }
 
 function w3_open() {
@@ -15,3 +15,22 @@ function w3_close() {
   document.getElementById("mySidebar").style.display = "none";
   document.getElementById("openNav").style.display = "inline-block";
 }
+
+ $(document).ready(function(){
+        $("#enterProfile").click(function(){
+            
+            $("#content").replaceWith(document.getElementById("profile").innerHTML);
+          	w3_close();
+        });
+ });
+
+
+ $(document).ready(function(){
+        $("#enterForm").click(function(){
+            
+            $("#content").replaceWith(document.getElementById("form").innerHTML);
+          	w3_close();
+        });
+    });
+
+ 
