2a9814751ff55bf46283ad4ef6d3a11d92a5a517
tashi21412 <tashi21412@gmail.com>
2017-10-28 14:51:26 -0400

    webstuff

---
+++ WebApp/homepage.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+	
+	document.getElementById("openNav").addEventListener("click",w3_open, false);
+document.getElementById("closeNav").addEventListener("click",w3_close, false);
+}
+
+function w3_open() {
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("mySidebar").style.width = "25%";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+}
+function w3_close() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+}
---
+++ WebApp/index.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+	
+		//document.getElementById("register").addEventListener("click",signUp, false);
+}
+
+function signUp(){
+ 	//var element = document.getElementById("signin");
+	//element.innerHTML = document.getElementById("signUp").innerHTML;
+
+
+
+}
