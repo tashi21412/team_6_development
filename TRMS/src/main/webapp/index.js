131fb733e19d44c76b08586b9551d224a6844645
tashi21412 <tashi21412@gmail.com>
2017-11-01 20:14:45 -0400

    correct project structure

--- TRMS/src/main/webapp/index.js
+++ TRMS/src/main/webapp/index.js
@@ -1,9 +1,44 @@
 window.onload = function(){
 	
-		//document.getElementById("register").addEventListener("click",signUp, false);
+	document.getElementById("register").addEventListener("click",signUp, false);
+
+
+}
+function checker(){
+	console.log("checked");
 }
 
 function signUp(){
- 
+
+	 if (document.getElementById("password").innerHTML.value != document.getElementById("confirmpassword").innerHTML.value){
+	 	
+		$("#error").show();
+	}
+ 	else {
+ 		console.log("correct password");
+ 		
+ 		var text = {"firstName":"John", "lastName":"Doe","username" : "johndoe" , "password":"mypassword"};
+ 		
+ 		//Step 1. Open XHR
+ 		var xhr = new XMLHttpRequest();
+ 		
+ 		//Step 2. function to handle ready state change of response
+ 		xhr.onreadystatechange = function() {
+ 			
+ 			if(xhr.readyState == 4 && xhr.status == 200){
+ 				
+ 				console.log(xhr.responseText);}
+ 			
+ 		}
+ 		
+ 		//Step 3. Open connection
+ 		xhr.open("GET", "http://localhost:8080/MYTRMS/RegistrationServlet", true);
+ 		
+ 		//Step 4. Send request
+ 		xhr.send();
+ 		
+		
+ 	}
 
 }
+
